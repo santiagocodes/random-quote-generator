8104795729943e7d3d8a58efb4ea5dcf806d5434
mc-santiago <mari.c.santiago@europe.com>
2020-04-09 21:28:29 +0200

    audit fix

--- src/App.js
+++ src/App.js
@@ -1,64 +1,73 @@
-import React, { Component } from 'react';
-import './App.css';
-import axios from 'axios';
-
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
 
 class RandomQuote extends Component {
-   constructor(props) {
-      super(props)
-      this.state = {
-         image:'',
-         quote: '',
-         character: ''
-      }
-   }
+  constructor(props) {
+    super(props);
+    this.state = {
+      image: "",
+      quote: "",
+      character: "",
+    };
+  }
 
-   componentDidMount() {
-      this.getQuote()
-   }
+  componentDidMount() {
+    this.getQuote();
+  }
 
-   getQuote() { 
-      let url = 'https://thesimpsonsquoteapi.glitch.me/quotes'
+  getQuote() {
+    let url = "https://thesimpsonsquoteapi.glitch.me/quotes";
 
-      axios.get(url)
-         .then(res => {
-            let data = res.data
-            console.log(data)
-            //let quoteNum = Math.floor(Math.random() * data.lenght)
-            let randomQuote = data[0]
+    axios.get(url).then((res) => {
+      let data = res.data;
+      console.log(data);
+      //let quoteNum = Math.floor(Math.random() * data.lenght)
+      let randomQuote = data[0];
 
-            this.setState({
-               image: randomQuote.image,
-               quote: randomQuote.quote,
-               character: randomQuote.character
-            })
-         })
-   }
+      this.setState({
+        image: randomQuote.image,
+        quote: randomQuote.quote,
+        character: randomQuote.character,
+      });
+    });
+  }
 
-   getNewQuote = () => { 
-      this.getQuote()
-   }
+  getNewQuote = () => {
+    this.getQuote();
+  };
 
-   render() {
-      const { image, quote, character } = this.state //Destructuring
-      return (
-        <div className="App">
-            
-            <h1 className='title'>Random Simpsons Quote</h1>
+  render() {
+    const { image, quote, character } = this.state; //Destructuring
+    return (
+      <div className="App">
+        <h1 className="title">Random Simpsons Quote</h1>
 
-            <div id='quote-box'>
-               <img className='quoteimg' src={image} alt='random simpsons character'/>
-               <div id='text'><p>"{quote}"</p></div>
-               <div id='text'><h5>- {character}</h5></div>
+        <div id="quote-box">
+          <img
+            className="quoteimg"
+            src={image}
+            alt="random simpsons character"
+          />
+          <div id="text">
+            <p>"{quote}"</p>
+          </div>
+          <div id="text">
+            <h5>- {character}</h5>
+          </div>
 
-               <div id='buttons'>
-               <button className= 'newQuoteButton buttons' onClick={this.getNewQuote}>New Quote</button>
-               </div>
-            </div>
-            
-         </div>
-      )
-   }
+          <div id="buttons">
+            <button
+              className="newQuoteButton buttons"
+              onClick={this.getNewQuote}
+            >
+              New Quote
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default RandomQuote
+export default RandomQuote;
